55ba79749b15d22b15d2a10941ae2812f8e0d745
wz-china <709044233@qq.com>
2019-10-04 22:54:32 +0800

    express造后端轮子模板

--- server/server.js
+++ server/server.js
@@ -1,66 +1,18 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const webpack = require('webpack')
-const webpackDevMiddleware = require('webpack-dev-middleware')
-const webpackHotMiddleware = require('webpack-hot-middleware')
-const WebpackConfig = require('./webpack.config')
+let express = require("express")
 
-const app = express()
-app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Methods', '*');
-  res.header('Content-Type', 'application/json;charset=utf-8');
-  next();
+const app = express();
+
+let wxpay = require("./routes/wxpay")
+
+import tsWxpay from "../src/ts-wxpay"
+console.log(tsWxpay)
+
+app.use("/pay",wxpay)
+
+app.get('/',function(req:any,res:any){
+  res.send('Hello World!');
 });
 
-const compiler = webpack(WebpackConfig)
-
-const router = express.Router()
-
-router.get('/simple/get', function(req, res) {
-  res.json({
-    msg: `hello world`
-  })
+app.listen(8888,function() {
+  console.log('Example app listening on port 8888!');
 })
-
-router.post('/simple/post', function(req, res) {
-  console.log(req.body)
-  res.json(req.body)
-})
-
-extendRouter()
-
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: '/__build__/',
-  stats: {
-    colors: true,
-    chunks: false
-  }
-}))
-
-app.use(webpackHotMiddleware(compiler))
-
-app.use(express.static(__dirname))
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-// 路由注册需要放在解析后面，否则body娶不到json数据
-app.use(router)
-
-const port = process.env.PORT || 7001
-module.exports = app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
-})
-
-function extendRouter() {
-  router.post('/extend/test', function(req, res) {
-    res.json(req.body)
-  })
-  router.get('/extend/get', function(req, res) {
-    res.json({msg:"ok"})
-  })
-
-
-}
